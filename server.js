353d7617c9392739a1f331140b8af6a124b1c87a
TanRai <tanvirraiyan456@gmail.com>
2024-01-17 07:03:25 +0600

    Added user endpoint

--- server.js
+++ server.js
@@ -1,44 +1,41 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const auth = require('./routes/auth');
-const http = require('http');
+const cors = require("cors");
+const auth = require("./routes/auth");
+const http = require("http");
 const { Server } = require("socket.io");
-const jwt = require('jsonwebtoken');
-const posts = require('./routes/posts');
-const authMiddleware = require('./middleware/auth');
+const jwt = require("jsonwebtoken");
+const posts = require("./routes/posts");
+const user = require("./routes/user");
+const authMiddleware = require("./middleware/auth");
 
-
-app.use(cors({
-  origin: 'http://localhost:5173',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:5173",
-    methods: ["GET", "POST"]
-  }
+    methods: ["GET", "POST"],
+  },
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
+io.on("connection", (socket) => {
+  console.log("a user connected");
 });
 
 app.use(express.json());
-app.use('/api/auth', auth);
-app.use('/api/posts', posts);
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use("/api/auth", auth);
+app.use("/api/posts", posts);
+app.use("/api/user", user);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.get('/api/user', authMiddleware,  (req, res) => {
-  res.send('Hello World!');
-});
-
-
-
-server.listen(3000, () => console.log('Server running on port 3000'));
+server.listen(3000, () => console.log("Server running on port 3000"));
